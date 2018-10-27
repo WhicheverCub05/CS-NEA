9d4e5585dcc140055da5231f6358e603dd3a166d
WhicheverCub05 <WhicheverCub05@gmail.com>
2018-10-27 23:25:43 +0100

    makes the graph logarithmic on the y axis and corrects the data potting
    also has a large, optional frequency band to test the response curve

---
+++ GUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+label = Label(root, text='this is too easy')
+
+label.pack()  # pack it in there the first place you fit it
+
+root.mainloop()  # to keep window open
