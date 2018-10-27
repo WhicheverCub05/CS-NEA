f2c76fdf0fd3e8b18de22df1c9fa30b7b75f7f73
WhicheverCub05 <WhicheverCub05@gmail.com>
2018-10-28 00:06:02 +0100

    learning to Fit widget within layout

--- GUI.py
+++ GUI.py
@@ -2,20 +2,11 @@ from tkinter import *
 
 root = Tk()
 
-topframe = Frame(root)
-topframe.pack()
-
-bottomframe = Frame(root)
-bottomframe.pack(side=BOTTOM)
-
-button1 = Button(topframe, text='Button 1', fg='red')
-button2 = Button(topframe, text='Button 2', fg='blue')
-button3 = Button(topframe, text='Button 3', fg='green')
-button4 = Button(topframe, text='Button 4', fg='black')
-
-button1.pack(side=LEFT)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
-button4.pack(side=BOTTOM)
+one = Label(root, text='one', bg='red', fg='white')
+one.pack()
+two = Label(root, text='two', bg='green', fg='black')
+two.pack(fill=X)
+three = Label(text='three', bg='blue', fg='white')
+three.pack(side=LEFT, fill=Y)
 
 root.mainloop()  # to keep window open
