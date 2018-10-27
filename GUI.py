2f3e0c6ea637da14abd1a7d37188e7f816807dcf
WhicheverCub05 <WhicheverCub05@gmail.com>
2018-10-27 23:58:55 +0100

    learning to organising my layout

--- GUI.py
+++ GUI.py
@@ -2,8 +2,20 @@ from tkinter import *
 
 root = Tk()
 
-label = Label(root, text='this is too easy')
+topframe = Frame(root)
+topframe.pack()
 
-label.pack()  # pack it in there the first place you fit it
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM)
+
+button1 = Button(topframe, text='Button 1', fg='red')
+button2 = Button(topframe, text='Button 2', fg='blue')
+button3 = Button(topframe, text='Button 3', fg='green')
+button4 = Button(topframe, text='Button 4', fg='black')
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack(side=BOTTOM)
 
 root.mainloop()  # to keep window open
