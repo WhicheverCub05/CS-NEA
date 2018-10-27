0514d3360d1102a7d563241690daf308271a270e
WhicheverCub05 <WhicheverCub05@gmail.com>
2018-10-28 00:26:37 +0100

    learning to bind functions to layouts

--- GUI.py
+++ GUI.py
@@ -2,18 +2,14 @@ from tkinter import *
 
 root = Tk()
 
-label1 = Label(root, text='name: ')
-label2 = Label(root, text='password: ')
-entry1 = Entry(root)
-entry2 = Entry(root)
 
-label1.grid(row=0, column=0, sticky=E)
-label2.grid(row=1, column=0, sticky=E)
+def printname(event):
+    print('My name is alex')
 
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
 
-checkbox = Checkbutton(root, text='Stay logged in?')
-checkbox.grid(columnspan=2)
+button1 = Button(root, text='Print name')
+button1.bind('<Button-1>', printname)
+button1.grid(row=0)
+
 
 root.mainloop()  # to keep window open
