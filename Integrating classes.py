83d6508ebaff40f8b47e94455920bef368488ee6
WhicheverCub05 <WhicheverCub05@gmail.com>
2019-02-24 04:49:50 +0000

    -Cleaned up code by removing unused elements/comments -Limited the UI's minimum size -Make the UI's default size wider -Pushed python print statements to the UI's log text box

--- Integrating classes.py
+++ Integrating classes.py
@@ -9,8 +9,8 @@ from scipy.io import wavfile as wav
 from tkinter import *
 
 
-# default_frequency_list = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
-default_frequency_list = [1000, 200, 400]  # for testing
+default_frequency_list = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
+# default_frequency_list = [1000, 200, 400]  for testing
 file = []
 i = 0
 sample_rate = 48000
@@ -23,10 +23,12 @@ root = Tk()
 frequency_checkbox_state = IntVar()
 user_input_list = StringVar()
 
+
 class UI:
 
     def __init__(self, master):
-        root.geometry("575x275")
+        root.geometry("700x275")
+        root.minsize(500, 275)
         # root.iconbitmap('icon1.ico')
         # root.title('Frequency Analyser')
 
@@ -69,7 +71,7 @@ class UI:
                                                      '           Step 3          ')
         self.setuptext.pack()
 
-        self.steptext1 = Label(self.setupframe, text='make sure the inline \n is outputting')
+        self.steptext1 = Label(self.setupframe, text='make sure your microphone \n is connected')
         self.steptext1.pack(side=LEFT)
 
         self.steptext3 = Label(self.setupframe, text='Run the sequence')
@@ -80,9 +82,8 @@ class UI:
         self.steptext2.pack()
 
         self.Checkbox = Checkbutton(self.startframe, state=ACTIVE, variable=frequency_checkbox_state,
-                                    text='Run custom \n frequencies?', height=3)
+                                    text='Run custom \n frequencies?', height=3, width = 14)
         self.Checkbox.pack(side=LEFT)
-
 
         self.frequencyinput = Entry(self.inputframe_left, textvariable=user_input_list, width=37)
         self.frequencyinput.pack(side=BOTTOM)
@@ -90,49 +91,30 @@ class UI:
         self.frequencytext = Label(self.inputframe_left, text='Input List - comma separated (,_)', height=2)
         self.frequencytext.pack(side=TOP)
 
-        self.logtext = Label(self.logframe, text='Log: ')
-        self.logtext.pack(side=TOP)
-
-        self.logtextbox = Text(self.logframe, width=30, height=5)
-        self.logtextbox.pack(side=RIGHT)
-
-        #self.graphtext = Label(self.bottomframe, text='')  #
-        #self.graphtext.pack(side=TOP)
-
         self.startButton = Button(self.startframe, text='Start', width=10, height=2, bg='#00B7FF')
         self.startButton.bind('<Button-1>', self.start_button)
         self.startButton.pack(side=LEFT)
 
-        #self.graphtext = Label(self.bottomframe, text='Graph')
+        self.logtext_title = Label(self.logframe, text='Log: ')
+        self.logtext_title.pack(side=TOP)
 
-        self.photo = PhotoImage(file='wave.png')
-        self.graph = Label(self.bottomframe, image=self.photo)
-
-        self.scrollbar = Scrollbar(self.bottomframe)
+        self.scrollbar = Scrollbar(self.logframe)
         self.scrollbar.pack(side=RIGHT, fill=Y)
 
-    #    def clear_image(self):
-    #       self.photo = Label(self.bottomframe, image='')
+        self.logtextbox = Text(self.logframe, width=45, height=5, yscrollcommand = self.scrollbar.set)
+        self.logtextbox.pack(side=LEFT)
 
-        self.graphtext = Label(self.graphframe, text='Graph')
+        self.scrollbar.config(command = self.logtextbox.yview)
 
-        self.graph = Label(self.bottomframe, image=self.photo)
+        def print_to_gui(printed_statements):
+            self.logtextbox.insert(INSERT, printed_statements)
 
-
-    def display_graph(self):
-        self.mainframe.pack_forget()
-        root.geometry("575x775")
-        self.graphtext.pack(side=TOP)
-        self.photo = PhotoImage(file='wave0.png')
-        self.graph.pack(side=BOTTOM)
+        sys.stdout.write = print_to_gui
 
     def start_button(self, root):
-        print("attempting to start")
-        # self.make_user_frequency_list()
-        # print('made the frequency list')
+        print("Starting")
         Mainclass.start(root)
-        # self.display_graph()
-        print("it went through")
+        print("End")
 
 
 class Mainclass:
@@ -142,11 +124,8 @@ class Mainclass:
 
     def make_user_frequency_list(self):
         print('make user f list input:', StringVar.get(user_input_list))
-        # self.user_frequency_list = '[' + StringVar.get(self.user_input_list) + ']'
-        # print('self.user_frequency_list b4 exec: ', self.user_frequency_list)
         user_frequency_list = StringVar.get(user_input_list)
         user_frequency_list = user_frequency_list.split(',')
-        # exec(self.user_frequency_list)
         print("self.user frequency_list = ", user_frequency_list)
 
         for i in range(len(user_frequency_list)):
@@ -156,25 +135,16 @@ class Mainclass:
         return user_frequency_list
 
     def determine_frequency_list(self):
-
-        # frequency_list = UI(root).make_user_frequency_list.__get__(object)
-        # print('frequency list at determine frequency list : ', )
-        # print("checkbox is on")
-
-        if frequency_checkbox_state.get() == 0:  # if UI(root).self.printCheckbox == FALSE:
+        if frequency_checkbox_state.get() == 0:
             frequency_list = default_frequency_list
             print('checkbox is off')
 
         else:
 
             frequency_list = Mainclass().make_user_frequency_list()
-            # frequency_list = UI.make_user_frequency_list.__get__(object, list)
             print("checkbox is on")
             print('frequency_list in dfl is ', frequency_list)
-        """
-            for i in range in len(frequency_list):
-                frequency_list[i].int
-        """
+
         return frequency_list
 
     def sinewave(self, frequency):
@@ -209,21 +179,12 @@ class Mainclass:
         plt.ylim(100000, 1000000000)
         plt.savefig('wave{}.png'.format(i))
 
-
-        #plt.savefig('wave.png') #  plt.savefig('wave{}.png'.format(i))
-
-
     def clear_files(self, file):
         for i in range(len(file)):
             os.remove(file[i])
 
-    # frequency list replaced default frequency list on every single occurrence
-
     def start(self):
         frequency_list = Mainclass().determine_frequency_list()
-        # Mainclass().frequency_list_name(frequency_list=Mainclass().make_frequency_list())
-        # print(Mainclass.make_frequency_list.frequency_list)
-        # for i in range(len(frequency_list)):
         i = 0
         print("frequency_list (i) in Mainclass().start: ", frequency_list[i])
         sd.wait()
@@ -239,17 +200,7 @@ class Mainclass:
         sd.wait()
 
 
-
-'''
-# potentually can be deleted
-
-    def show_graph(self, i): # was in Mainclass
-        Mainclass().plot_graph(recording=Mainclass().play_rec(sinewave=Mainclass().sinewave(frequency=default_frequency_list[i]), frequency=default_frequency_list[i]), i=i)
-        print('show_graph i: ', i)
-'''
-
 if __name__ == "__main__":
-    a = UI(root)  # .Appearance()
+    a = UI(root)
     root.mainloop()
 
-# try to make the frequency list work out in the start, make_user_frequency_list
